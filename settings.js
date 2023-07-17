db129e2cf8f95a090b90c8510adc4872abc13ade
Mentalcase12 <109283519+Mentalcase12@users.noreply.github.com>
2023-07-17 13:45:03 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,17 +21,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['27764272140'] //ur owner number
+global.ownernomer = "27764272140" //ur owner number2
+global.ownername = "🦄Mental Case" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
+global.botname = "Mentalcase Bot MD V8"
+global.ownernumber = '27764272140'
+global.ownername = '🦄드림 가이 Mentalcase'
 global.ownerNumber = ["916909137213@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
